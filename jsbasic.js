a1c794a1d5a2d110da98a32718129da0d23bd1be
dennis <dennisviphen@gmail.com>
2023-09-01 19:07:09 +0530

    html push

---
+++ jsbasic.js
@@ -0,0 +1,134 @@
+
+// function sum(a)
+// {
+//      a['age']=40
+//      a['name']='ajay'                // changing value by object
+
+// }
+
+// a={'name':'arun','age':'28'}               // passing value by object 
+// sum(a)
+// console.log(a)
+
+
+
+// // anonymous function  ========function without name
+
+// a=function(x,y,z)
+//     {
+//         return x+y+z
+//     }
+// console.log(a(2,2,2))
+
+
+//arrow function
+
+// s=[
+//     {'name':'ajar',
+//       'age':20},
+//       {
+//       'name':'arun',
+//       'age':23},
+//       {
+//         'name':'max',
+//         'age':25},
+//         {
+//             'name':'raj',
+//             'age':30}
+//     ]
+
+    // s.forEach(e=>console.log(e))
+
+    // f=s.filter( e=> e.age>=21)
+    // console.log(f)
+
+
+    // d=[1,4,6,5]
+    // sum=d.reduce((a,b)=>a+b)
+    // console.log(sum)
+  
+//  news = s.map(e=> e.sal=e.age*1000)
+
+// console.log(s) 
+ 
+// s=[
+//     { name: 'ajar', age: 20, sal: 20000 },
+//     { name: 'arun', age: 23, sal: 23000 },
+//     { name: 'max', age: 25, sal: 25000 },
+//     { name: 'raj', age: 30, sal: 30000 }
+//   ]
+//   total=s.reduce((a,b)=>a.sal+b.sal)
+//   console.log(total)
+///======================================================================================================================================================================================================
+     
+//MAP
+// //1. square of each number using map
+//  a=[2,3,4,5]
+//  result=a.map(e=>e*e);
+//  console.log(a);
+//  console.log(result);   
+ 
+//  //2.Write a function that takes an array of objects with a name property and returns a new array of strings with the name of each object capitalized. Use the Map object to accomplish this
+//  a=["denni","vibi","jo","jabi"]
+//  result=a.map(e=>e.toUpperCase())
+//  console.log(result);
+//  console.log(a)
+
+
+
+////filter
+
+// //even number
+
+// a=[2,3,4,5,6]
+// t=a.filter(e=>e%2==0)
+// console.log(t)
+
+//2..
+a=[]
+
+
+
+
+
+
+
+
+
+
+
+
+    //// for each
+  
+  ////1.. sum of 2 array
+//     sum=0,sum1=0
+//     a=[2,3,4,5] ,b=[1,2,3,4]
+//     a.forEach(e=>{sum=sum+e})
+//     console.log(sum)
+//     b.forEach(e=>{sum1=sum1+e})
+//     console.log(sum1)
+//  console.log(sum+sum1)
+
+
+//    ////2..even number
+//    a=[1,2,3,4,5,6]
+//    a.forEach(e => {if(e%2==0){console.log(e) }  
+//    });
+
+// ////3..reverse  a order
+ a=[1,2,3,4]
+ a.forEach(e=>{a.unshift(e)})
+ console.log(e)
+
+//  //4..
+//  a=[1,2,3,4]
+//  b=[2,3,4,6]
+  
+//  a.forEach(e=>e.a[i]+e.b[i])
+
+
+// n=4
+// n.forEach(e => {
+    
+// });
+   
